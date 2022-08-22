41e2266e686c78d9a2f794417f667ee233d97a17
Yuriy Sviridov <crip.shooter@gmail.com>
2022-08-22 06:55:11 +0300

    add Main

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    static String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+            "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    public static void main(String[] args) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < text.length(); i++) {
+            char sym = text.toLowerCase().charAt(i);
+            if (!map.containsKey(sym)) {
+                map.put(sym, 1);
+            } else {
+                int value = map.get(sym);
+                value++;
+                map.put(sym, value);
+            }
+        }
+
+
+        int max = -1;
+        int min = Integer.MAX_VALUE;
+        for (Map.Entry<Character, Integer> kv : map.entrySet()) {
+            if (kv.getValue() > max) {
+                max = kv.getValue();
+            } else if (kv.getValue() < min) {
+                min = kv.getValue();
+            }
+        }
+        System.out.println("max: " + max);
+        System.out.println("min: " + min);
+    }
+}
